9d0e7393e0faf0a1a0a52bc3a181a478d933ee88
juandre1298 <juandre1298@gmail.com>
2023-03-15 10:20:46 -0500

    product details page structure and style done

--- e-commerce/src/components/Product.js
+++ e-commerce/src/components/Product.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
-
+import { MdOutlineStar } from "react-icons/md";
 const Product = () => {
   const [details, setDetails] = useState({});
   const location = useLocation();
@@ -25,10 +25,46 @@ const Product = () => {
             )}
           </div>
         </div>
-        <div className="w-3/5">
+        <div className="w-3/5 flex flex-col justify-center gap-12">
           <div>
             <h2 className="text-4x1 font-semibold">{details.title}</h2>
+            <div className="flex items-center gap-4 mt-3">
+              <p className="line-through text-gray-500">${details.oldPrice}</p>
+              <p className="font-semibold">${details.price}</p>
+            </div>
           </div>
+          <div className="flex item-center gap-2 text-base">
+            <div className="flex">
+              <MdOutlineStar />
+              <MdOutlineStar />
+              <MdOutlineStar />
+              <MdOutlineStar />
+              <MdOutlineStar />
+            </div>
+            <p className="text-xs text-gray-500">(1 Customer review)</p>
+          </div>
+          <p className="text-xs text-gray-500 -mt-3">{details.description}</p>
+          <div className="flex gap-4">
+            <div className="w-52 flex items-center justify-between text-gray-500 gap-4 border p-3">
+              <p className="text-sm">Qyuantity</p>
+              <div className="flex items-center gap-4 text-sm font-semibold">
+                <button className="border h-5 font-normal text-lg flex items-center justify-center px-2 hover:bg-gray-700 hover:text-white cursor-pointer duration-300 active:bg-black">
+                  -
+                </button>
+                <span>{1}</span>
+                <button className="border h-5 font-normal text-lg flex items-center justify-center px-2 hover:bg-gray-700 hover:text-white cursor-pointer duration-300 active:bg-black">
+                  +
+                </button>
+              </div>
+            </div>
+            <button className="bg-black text-white py-3 px-6 active:bg-gray-800">
+              add to cart
+            </button>
+          </div>
+          <p className="text-base text-gray-500">
+            Category:{" "}
+            <span className="font-medium capitalize">{details.category} </span>
+          </p>
         </div>
       </div>
     </div>
