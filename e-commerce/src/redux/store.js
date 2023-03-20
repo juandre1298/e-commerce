4fd2875e252b555950f9d1c33549d787d2d9a86c
juandre1298 <juandre1298@gmail.com>
2023-03-20 16:21:40 -0500

    added redux persist featuer

--- e-commerce/src/redux/store.js
+++ e-commerce/src/redux/store.js
@@ -1,8 +1,34 @@
 import { configureStore } from "@reduxjs/toolkit";
 import shopReducer from "./shopSlice";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, shopReducer);
+
 export const store = configureStore({
-  reducer: {
-    shop: shopReducer,
-  },
+  reducer: { shop: persistedReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export let persistor = persistStore(store);
